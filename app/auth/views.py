bc76158f4470ed64224eba96e8e5643ed7a9470c
Adam Vilmin <crappyoats@gmail.com>
2017-11-26 19:16:30 -0600

    admin logouts added

--- app/auth/views.py
+++ app/auth/views.py
@@ -17,7 +17,7 @@ def login():
         if user is not None and user.verify_password(form.password.data):
             login_user(user)
 
-            if not user.in_lab:
+            if not user.in_lab and user.user_type != 'staff':
                 return redirect(request.args.get('next') or
                                 url_for('main.log_visit',
                                         username=user.username))
--- app/main/views.py
+++ app/main/views.py
@@ -11,30 +11,21 @@ from .. import db
 @main.route('/', methods=['GET', 'POST'])
 @main.route('/index')
 def index():
-    all_users = User.query.filter_by(in_lab=True)
-    if not all_users:
-        print("thats the problem")
+    all_users = User.query.filter(db.and_(User.in_lab == 1,
+                                          User.user_type != 'staff'))
+
     return render_template('index.html', users=all_users)
 
 
 @main.route('/user/<username>', methods=['GET', 'POST'])
 @login_required
 def user(username):
-    form = VisitForm()
     user = current_user._get_current_object()
-
+    all_users = User.query.filter(db.and_(User.in_lab == 1,
+                                          User.user_type != 'staff'))
     if user.username != username:
         abort(500)
-    if form.validate_on_submit():
-        visit = Visit(purpose=form.purpose.data,
-                      visit_user=user.id,
-                      in_time=datetime.now(),
-                      type_visit=form.type_visit.data)
-
-        db.session.add(visit)
-        db.session.commit()
-        return redirect(url_for('main.user', username=username))
-    return render_template('user.html', user=user, form=form)
+    return render_template('user.html', user=user, all_users=all_users)
 
 
 @main.route('/log_visit/<username>', methods=['GET', 'POST'])
@@ -42,7 +33,9 @@ def user(username):
 def log_visit(username):
     form = VisitForm()
     user = current_user._get_current_object()
-
+    all_users = all_users = User.query.filter(db.and_(User.in_lab == 1,
+                                                      User.user_type ==
+                                                      'staff'))
     if user.username != username:
         abort(500)
     if form.validate_on_submit():
@@ -54,4 +47,19 @@ def log_visit(username):
         db.session.add(visit)
         db.session.commit()
         return redirect(url_for('main.user', username=username))
-    return render_template('log_visit.html', user=user, form=form)
+    return render_template('log_visit.html', user=user, all_users=all_users)
+
+
+@main.route('/user/<username>/admin_logout', methods=['GET', 'POST'])
+@login_required
+def admin_logout(username):
+    user = current_user._get_current_object()
+    if user.user_type != 'staff':
+        abort(500)
+
+    loggin_out = User.query.filter_by(username=username).first()
+    loggin_out.in_lab = False
+    loggin_out.out_time = datetime.now()
+    all_users = User.query.filter(db.and_(User.in_lab == 1,
+                                          User.user_type != 'staff'))
+    return render_template('user.html', user=user, all_users=all_users)
